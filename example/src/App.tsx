69d3b0c487a6c25278d9ffd7942dc4d23588ba8c
Simon Kirekov <kirekov@gmail.com>
2020-08-07 21:13:37 +0700

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'great-hooks'
+import 'great-hooks/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
