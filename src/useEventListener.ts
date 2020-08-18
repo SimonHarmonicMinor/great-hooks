8703e3bb77b44cf4a91032c050382650549388ef
Semyon Kirekov <kirekov@gmail.com>
2020-08-18 17:14:55 +0700

    Added useEventListener hook

--- src/useEventListener.ts
+++ src/useEventListener.ts
@@ -1,3 +1,28 @@
-function useEventListener() {}
+import { useAutoUpdateRef } from './useAutoUpdateRef'
+import { useEffect } from 'react'
+
+type UseEventListenerParams = {
+  eventName: string
+  onEventTriggered: (event: Event) => any
+  element: EventTarget
+}
+
+function useEventListener({
+  eventName,
+  onEventTriggered,
+  element
+}: UseEventListenerParams) {
+  const savedEventCallback = useAutoUpdateRef<(event: Event) => any>(
+    onEventTriggered
+  )
+  useEffect(() => {
+    const eventListener = (event: Event) => savedEventCallback.current(event)
+    element.addEventListener(eventName, eventListener)
+    return () => {
+      element.removeEventListener(eventName, eventListener)
+    }
+  }, [eventName, element])
+}
 
 export { useEventListener }
+export type { UseEventListenerParams }
