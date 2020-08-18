83028f51da01a36a3c784b6a4d19a87ad86a3707
Semyon Kirekov <kirekov@gmail.com>
2020-08-18 11:29:03 +0700

    Changed the "deploy" behaviour in order to read the version from "package.json"

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,10 @@
-yarn release
+#!/bin/sh
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+yarn publish --access public --new-version "$PACKAGE_VERSION"
