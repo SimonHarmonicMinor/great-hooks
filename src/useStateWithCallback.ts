9eaabc2dae094986545d19a750f465ee8c39eaa2
Semyon Kirekov <kirekov@gmail.com>
2020-08-20 15:09:38 +0700

    Added useStateWithCallback hook

--- src/useStateWithCallback.ts
+++ src/useStateWithCallback.ts
@@ -1,3 +1,29 @@
-function useStateWithCallback() {}
+import { SetStateAction, useEffect, useRef, useState } from 'react'
+
+function useStateWithCallback<T>(initialState: T) {
+  const [state, setState] = useState(initialState)
+  const callbackRef = useRef<SetStateCallback<T> | null>(null)
+
+  const setStateWithCallback = (
+    setStateAction: SetStateAction<T>,
+    callback: SetStateCallback<T>
+  ) => {
+    callbackRef.current = callback
+    setState(setStateAction)
+  }
+
+  useEffect(() => {
+    if (callbackRef.current) {
+      callbackRef.current(state)
+      callbackRef.current = null
+    }
+  }, [state])
+
+  return [state, setStateWithCallback]
+}
+
+type SetStateCallback<T> = {
+  (state: T): any
+}
 
 export { useStateWithCallback }
