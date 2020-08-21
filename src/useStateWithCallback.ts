072602248221a99ad4621033a6177b08ad3d8cc6
Semyon Kirekov <kirekov@gmail.com>
2020-08-21 19:09:42 +0700

    Added typings for return value of useStateWithCallback

--- src/useStateWithCallback.ts
+++ src/useStateWithCallback.ts
@@ -1,16 +1,16 @@
-import { SetStateAction, useEffect, useRef, useState } from 'react'
+import { SetStateAction, useCallback, useEffect, useRef, useState } from 'react'
 
-function useStateWithCallback<T>(initialState: T) {
+function useStateWithCallback<T>(initialState: T): [T, SetState<T>] {
   const [state, setState] = useState(initialState)
   const callbackRef = useRef<SetStateCallback<T> | null>(null)
 
-  const setStateWithCallback = (
-    setStateAction: SetStateAction<T>,
-    callback: SetStateCallback<T>
-  ) => {
-    callbackRef.current = callback
-    setState(setStateAction)
-  }
+  const setStateWithCallback: SetState<T> = useCallback(
+    (setStateAction: SetStateAction<T>, callback: SetStateCallback<T>) => {
+      callbackRef.current = callback
+      setState(setStateAction)
+    },
+    []
+  )
 
   useEffect(() => {
     if (callbackRef.current) {
@@ -20,6 +20,10 @@ function useStateWithCallback<T>(initialState: T) {
   }, [state])
 
   return [state, setStateWithCallback]
+}
+
+type SetState<T> = {
+  (setStateAction: SetStateAction<T>, callback: SetStateCallback<T>): void
 }
 
 type SetStateCallback<T> = {
