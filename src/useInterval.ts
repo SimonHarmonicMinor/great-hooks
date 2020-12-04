985881ae34fc442bcb5211cb18cb68cfea4cffc9
Semyon Kirekov <semyon@kirekov.com>
2020-12-04 21:45:26 +0300

    [#34] - useInterval stores 'interval' and 'delay' in refs

--- src/useInterval.ts
+++ src/useInterval.ts
@@ -1,7 +1,7 @@
-import { useEffect, useRef } from 'react'
+import { useCallback, useEffect, useRef } from 'react'
 import { useAutoUpdateRef } from './useAutoUpdateRef'
 
-type UseIntervalParams = {
+export interface UseIntervalParams {
   callback: () => any
   interval: number
   delay?: number
@@ -19,39 +19,39 @@ type UseIntervalParams = {
  *
  * @throws RangeError if <code>interval</code> or <code>delay</code> is less than 0
  */
-function useInterval({ callback, interval, delay }: UseIntervalParams) {
+export function useInterval({ callback, interval, delay }: UseIntervalParams) {
   if (interval < 0 || (delay ?? 0) < 0) {
     throw new RangeError(
       `"interval" and "delay" parameters should be non-negative. Given: [interval=${interval}, delay=${delay}]`
     )
   }
   const savedCallback = useAutoUpdateRef<() => any>(callback)
+  const savedInterval = useAutoUpdateRef<number>(interval)
+  const savedDelay = useAutoUpdateRef<number | undefined>(delay)
+
   const savedTimerId = useRef<NodeJS.Timeout>()
+
+  const callInLoop = useCallback(() => {
+    savedCallback.current()
+    const nextIteration = () =>
+      (savedTimerId.current = setTimeout(callInLoop, savedInterval.current))
+    nextIteration()
+  }, [savedCallback, savedInterval])
 
   useEffect(() => {
     let delayedTimerId: NodeJS.Timeout
-    if (!delay) {
+    if (!savedDelay.current) {
       callInLoop()
     } else {
-      delayedTimerId = setTimeout(callInLoop, delay)
+      delayedTimerId = setTimeout(callInLoop, savedDelay.current)
     }
     return () => {
-      // @ts-ignore
-      clearTimeout(savedTimerId.current)
+      if (savedTimerId.current) {
+        clearTimeout(savedTimerId.current)
+      }
       if (delayedTimerId) {
         clearTimeout(delayedTimerId)
       }
     }
-  }, [interval, delay])
-
-  function callInLoop() {
-    savedCallback.current()
-    const nextIteration = () => {
-      savedTimerId.current = setTimeout(callInLoop, interval)
-    }
-    nextIteration()
-  }
+  }, [callInLoop, savedTimerId, savedDelay])
 }
-
-export { useInterval }
-export type { UseIntervalParams }
